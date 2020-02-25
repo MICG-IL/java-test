a112f0142af4dfa57439ad80291feab806567243
mcarlson-sb <mcarlson.ucsb@gmail.com>
2020-02-25 13:11:49 -0800

    make sure tests are setup and working

---
+++ src/test/java/BasketTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BasketTest {
+    @Test
+    public void firstTest() {
+        Basket basket = new Basket();
+        assert(basket != null);
+    }
+
+}
